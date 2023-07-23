d4f321fea9e39c22b70a3e8779f335e308f3bf5b
Satria Adi Permana <67152028+satriaadiper@users.noreply.github.com>
2023-07-23 10:28:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,39 @@
+# main.py
+
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import downloader
 import uploader
+from config import TELEGRAM_BOT_TOKEN
 
 # Enable logging
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
 logger = logging.getLogger(__name__)
 
-# Define a few command handlers
 def start(update, context):
     update.message.reply_text('Hi! I can download and upload videos for you.')
-
-def help(update, context):
-    update.message.reply_text('/help - Get help\n/about - About this bot')
-
-def about(update, context):
-    update.message.reply_text('I am a bot that can download and upload videos!')
 
 def handle_video(update, context):
     file_id = update.message.video.file_id
     file_path = downloader.download_video(file_id)
-    upload_result = uploader.upload_video(file_path)
-    update.message.reply_text(f"Video uploaded! Filecode: {upload_result['filecode']}")
+    file_info = uploader.upload_video(file_path)
+
+    if file_info:
+        update.message.reply_text(f"Video uploaded! Filecode: {file_info['filecode']}")
+    else:
+        update.message.reply_text("Error uploading video")
 
 def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 def main():
-    TELEGRAM_BOT_TOKEN = '6068957387:AAEBNTiYX83G54iaOGl5xPPKPdUsuDnH2mw'
     updater = Updater(TELEGRAM_BOT_TOKEN)
-
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("about", about))
-
     dp.add_handler(MessageHandler(Filters.video, handle_video))
-
     dp.add_error_handler(error)
 
     updater.start_polling()
