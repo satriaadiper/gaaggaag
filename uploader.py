c1daaf0d2d5428d2ba0f7039ed57f90018267597
Satria Adi Permana <67152028+satriaadiper@users.noreply.github.com>
2023-07-23 10:29:58 +0700

    Update uploader.py

--- uploader.py
+++ uploader.py
@@ -1,9 +1,25 @@
-import requests
+import os
+from doodstream import DoodStream
 from config import API_KEY
+import downloader
 
-def upload_video(file_path):
-    url = f'https://doodapi.com/api/upload/server?key={API_KEY}'
-    files = {'file': open(file_path, 'rb')}
-    response = requests.post(url, files=files)
-    response.raise_for_status()
-    return response.json()
+DOWNLOAD_FOLDER = 'downloads'
+
+if not os.path.exists(DOWNLOAD_FOLDER):
+    os.makedirs(DOWNLOAD_FOLDER)
+
+d = DoodStream()
+d.api_key = API_KEY
+
+def upload_video(file_id):
+    file_path = os.path.join(DOWNLOAD_FOLDER, f"{file_id}.mp4")
+
+    downloader.download_video(file_id, file_path)
+
+    upload_result = d.local_upload(file_path)
+
+    file_id = upload_result['id']
+
+    file_info = d.file_info(file_id)
+
+    return file_info
